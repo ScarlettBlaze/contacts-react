f0406d312f33fdd34e83bef09ea859c395a6ff76
Andraea <andraea.camble@gmail.com>
2018-11-11 20:09:13 -0600

    Separated CreateContact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
+    screen: 'list', //list, create
     contacts: []
   }
 
@@ -23,12 +24,16 @@ class App extends Component {
 
  render() {
    return (
-     <div>
-       <ListContacts 
-          onDeleteContact= {this.removeContact} 
-          contacts={this.state.contacts}
-       />
-       <CreateContact />
+     <div className="app">
+        {this.state.screen === 'list' && (
+          <ListContacts 
+            onDeleteContact= {this.removeContact} 
+            contacts={this.state.contacts}
+          />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+        )}
      </div>
    )
   }
